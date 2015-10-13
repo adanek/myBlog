03f7eb5f6e05b17d7d7a80f548b23fa587400e7b
Pati <patrick.deutsch@outlook.com>
2015-10-13 22:04:04 +0200

    Test_Pati

--- app/articles/article.php
+++ app/articles/article.php
@@ -3,6 +3,7 @@
 class Article {
 	
 	private $title;
+	private $author;
 	
 	public function get_title(){
 		return $this->title;
@@ -10,6 +11,14 @@ class Article {
 	
 	public function set_title($param1){
 		$this->title = $param1;
+	}
+	
+	public function get_author(){
+		return $this->author;
+	}
+	
+	public function set_author($param1){
+		$this->author = $param1;
 	}
 	
 }

07108b666fa3b5303aeeef13df9c30815ff1e7f7
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-10-13 21:47:25 +0200

    Add edit article route

--- app/articles/edit.php
+++ app/articles/edit.php
@@ -3,9 +3,8 @@
  * Created by IntelliJ IDEA.
  * User: adanek
  * Date: 13.10.2015
- * Time: 21:14
+ * Time: 21:34
  */
-
-$state = 'create';
+$state = 'edit';
 $page_content = '../views/articles/edit.php';
 include('../_layout.php');

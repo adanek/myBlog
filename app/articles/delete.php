3501719e226f852f39cb706b58d98ccb8c6350b1
Pati <patrick.deutsch@outlook.com>
2015-10-14 16:13:52 +0200

    Article_delete_test

--- app/articles/delete.php
+++ app/articles/delete.php
@@ -5,6 +5,17 @@ $request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 if($method == 'DELETE'){
 
    parse_str(file_get_contents("php://input"), $post_vars);
+   
+   while($art = current($articles)){
+   	$id = $art->get_id();
+   	if($id == $post_vars["id"]){
+   		unset($articles[$id]);
+   		break;
+   	}
+   	next($articles);
+   }
+   
+   
     echo('Article Nr. ' .$post_vars["id"] .' deleted');
 }
 

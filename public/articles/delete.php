6d707e28726bd58f35451cd44658704310a6cab4
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-10-22 21:52:55 +0200

    Add user control to article delete

--- public/articles/delete.php
+++ public/articles/delete.php
@@ -2,9 +2,11 @@
 include_once('../../app/services/session.php');
 include_once('../../app/models/article.php');
 include_once('../../app/services/ArticleService.php');
+include_once('../../app/services/AuthenticationService.php');
+include_once('../../app/services/HttpService.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+
 
 if($method == 'DELETE'){
 
@@ -13,8 +15,16 @@ if($method == 'DELETE'){
     if(isset($post_vars['id'])){
         $id = $post_vars['id'];
 
-        $res = ArticleService::get_instance()->remove_article($id);
-        echo($res);
+        if(!AuthenticationService::can_delete_article($id)){
+            HttpService::return_unauthorized();
+        }
+
+        ArticleService::get_instance()->remove_article($id);
+        exit();
     }
 }
+
+//Otherwise
+HttpService::return_not_found();
+
 ?>

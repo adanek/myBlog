bb6f912054e9b3f663f5fee805cfee5d6b0eb7b9
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-10-22 12:07:07 +0200

    Add client functionality for adding comments

---
+++ public/scripts/comment.js
@@ -0,0 +1,50 @@
+function showForm() {
+    var form = document.getElementById('form-comment');
+    form.classList.remove('hidden');
+
+    var btn = document.getElementById('btn-form-show');
+    btn.classList.add('hidden');
+}
+
+function hideForm() {
+    var form = document.getElementById('form-comment');
+    form.classList.add('hidden');
+
+    var btn = document.getElementById('btn-form-show');
+    btn.classList.remove('hidden');
+}
+
+
+function addComment(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            var list = document.getElementById('comments');
+            var e = document.createElement('div');
+            e.innerHTML = req.responseText;
+            list.insertBefore(e, list.firstChild);
+            hideForm();
+        }
+    };
+
+    var form = document.getElementById('form-comment');
+    var data = new FormData(form);
+
+    req.open('POST', '/articles/show.php', true);
+    req.send(data);
+
+    return false;
+}
+
+(function () {
+    var form = document.getElementById('form-comment');
+
+    if (form.attachEvent) {
+        form.attachEvent('submit', addComment);
+    }
+    else {
+        form.addEventListener('submit', addComment);
+    }
+})();
